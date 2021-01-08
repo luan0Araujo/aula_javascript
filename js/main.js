d6f932768234ea156e1390f4d81dc8caf94a6f00
Luan Araujo <luanaraujo59@gmail.com>
2021-01-08 19:37:22 -0300

    Condicional, Laços e Date

--- js/main.js
+++ js/main.js
@@ -34,3 +34,36 @@ console.log(lista.length);
 console.log(lista.reverse());
 console.log(lista.toString());
 console.log(lista.join(" - "));*/
+
+/*Condicional
+var idade = prompt();
+
+if(idade >= 18){
+	alert("Maior de idade");
+}
+else{
+	alert("Menor de idade");
+}
+*/
+
+
+/*Laços
+var cont=0;
+var i;
+
+while(cont<=5){
+	alert(cont);
+	cont++;
+}
+for(i=1; i<=5; i++){
+	alert(i);
+	
+}
+*/
+
+var data = new Date();
+
+alert(data);
+alert(data.getDay());
+alert(data.toLocaleDateString());
+alert(data.toLocaleString());
