a4bfe1d59bdfc49d85f09fd5c29267830133d9fd
Luan Araujo <luanaraujo59@gmail.com>
2021-01-08 15:45:50 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+var nome="Luan de Araujo";
+var idade=21;
+var n1=7;
+var n2=5;
+var curso="Analise e Desenvolvimento de Sistemas";
+alert(nome + " tem " + idade + " anos");
+console.log("resultado: " + n1 * n2);
+console.log(curso.toUpperCase());
+console.log(curso.replace("Analise", "3"));
+*/
