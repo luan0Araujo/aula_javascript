787030fd25a0582a493e05a932f5566c6e1ce015
Luan Araujo <luanaraujo59@gmail.com>
2021-01-08 16:12:14 -0300

    Array e Dicionario

--- js/main.js
+++ js/main.js
@@ -9,3 +9,28 @@ console.log("resultado: " + n1 * n2);
 console.log(curso.toUpperCase());
 console.log(curso.replace("Analise", "3"));
 */
+
+
+
+/*Dicionario
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+*/
+
+/*Lista de Dicionarios
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}, {nome: "limão", cor: "verde"}];
+console.log(frutas);
+console.log(frutas[2].cor);
+*/
+
+/*Array
+var lista = ["maça", "pera", "laranja"];
+
+console.log(lista);
+lista.push("uva");
+lista.pop();
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));*/
